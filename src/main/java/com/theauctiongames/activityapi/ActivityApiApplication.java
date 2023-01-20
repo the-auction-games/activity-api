0e5327eee7a0208f82f773eeffe24c78e6530cb9
Joel Smith <joelsmith.2019@gmail.com>
2023-01-20 11:20:15 -0700

    Initial Commit

---
+++ src/main/java/com/theauctiongames/activityapi/ActivityApiApplication.java
@@ -0,0 +1,21 @@
+package com.theauctiongames.activityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main class for the activity api.
+ */
+@SpringBootApplication
+public class ActivityApiApplication {
+
+    /**
+     * The main method to start the activity api.
+     *
+     * @param args the command line args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/theauctiongames/activityapi/config/SpringConfig.java
@@ -0,0 +1,11 @@
+package com.theauctiongames.activityapi.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The spring configuration class.
+ */
+@Configuration
+public class SpringConfig {
+    // Currently Unused
+}
