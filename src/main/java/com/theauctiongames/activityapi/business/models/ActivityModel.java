70c783b56f9312a7c7f0e79697b99d6e1633a285
Joel Smith <joelsmith.2019@gmail.com>
2023-01-20 11:48:37 -0700

    Add entities and models

---
+++ src/main/java/com/theauctiongames/activityapi/business/models/ActivityModel.java
@@ -0,0 +1,64 @@
+package com.theauctiongames.activityapi.business.models;
+
+import com.theauctiongames.activityapi.data.entities.ActivityEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * A model object for handling activities.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityModel {
+
+    /**
+     * Construct an activity model from an activity entity.
+     *
+     * @param entity the activity entity
+     * @return the activity model
+     */
+    public static ActivityModel fromEntity(ActivityEntity entity) {
+        return new ActivityModel(
+                entity.getId(),
+                entity.getUserId(),
+                entity.getActivityType(),
+                entity.getActivityDescription(),
+                entity.getUrlRedirect(),
+                entity.getTimestamp()
+        );
+    }
+
+    /**
+     * The activity's id.
+     */
+    private String id;
+
+    /**
+     * The user id involved in the activity.
+     */
+    private String userId;
+
+    /**
+     * The activity's type.
+     */
+    private String activityType;
+
+    /**
+     * The activity's description.
+     */
+    private String activityDescription;
+
+    /**
+     * The redirect url to the activity.
+     */
+    private String urlRedirect;
+
+    /**
+     * The activity's timestamp.
+     */
+    private long timestamp;
+}
---
+++ src/main/java/com/theauctiongames/activityapi/data/entities/ActivityEntity.java
@@ -0,0 +1,64 @@
+package com.theauctiongames.activityapi.data.entities;
+
+import com.theauctiongames.activityapi.business.models.ActivityModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * A database entity object for handling activities.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityEntity {
+
+    /**
+     * Construct an activity entity from an activity model.
+     *
+     * @param model the activity model
+     * @return the activity model
+     */
+    public static ActivityEntity fromEntity(ActivityModel model) {
+        return new ActivityEntity(
+                model.getId(),
+                model.getUserId(),
+                model.getActivityType(),
+                model.getActivityDescription(),
+                model.getUrlRedirect(),
+                model.getTimestamp()
+        );
+    }
+
+    /**
+     * The activity's id.
+     */
+    private String id;
+
+    /**
+     * The user id involved in the activity.
+     */
+    private String userId;
+
+    /**
+     * The activity's type.
+     */
+    private String activityType;
+
+    /**
+     * The activity's description.
+     */
+    private String activityDescription;
+
+    /**
+     * The redirect url to the activity.
+     */
+    private String urlRedirect;
+
+    /**
+     * The activity's timestamp.
+     */
+    private long timestamp;
+}
